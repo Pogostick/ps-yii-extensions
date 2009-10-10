c1a72f843219d447ac442d2b0fa9bfa5bd084694
Jerry Ablan <jablan@lucifer>
2009-10-10 19:49:49 +0000

    CKEditor v1.0

--- extensions/pogostick/external/ckeditor/config.js
+++ extensions/pogostick/external/ckeditor/config.js
@@ -7,5 +7,5 @@ CKEDITOR.editorConfig = function( config )
 {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	// config.skin = 'office2003';
+	// config.uiColor = '#AADC6E';
 };
