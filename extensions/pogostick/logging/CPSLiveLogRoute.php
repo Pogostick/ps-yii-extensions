7ec4a52e359836d5533fc987f5240a63b24662c5
Jerry Ablan <jablan@silverpop.com>
2011-02-25 18:56:49 -0500

    Removed setting autoFlush

--- extensions/pogostick/logging/CPSLiveLogRoute.php
+++ extensions/pogostick/logging/CPSLiveLogRoute.php
@@ -36,29 +36,18 @@ class CPSLiveLogRoute extends CFileLogRoute
 	protected $_categoryWidth = 40;
 	/**
 	 * Get the minimum width of the category column in the log output
-	 * @return integer 
+	 * @return integer
 	 */
 	public function getCategoryWidth() { return $this->_categoryWidth; }
 	/**
 	 * Set the minimum width of the category column in the log output
-	 * @return integer 
+	 * @return integer
 	 */
 	public function setCategoryWidth( $value ) { $this->_categoryWidth = $value; }
 
 	//********************************************************************************
 	//* Public Methods
 	//********************************************************************************
-
-	/**
-	 * Initialize component
-	 */
-	public function init()
-	{
-		parent::init();
-
-		//	Write each line out to disk
-		Yii::getLogger()->autoFlush = 1;
-	}
 
 	/**
 	 * Retrieves filtered log messages from logger for further processing.
