0548b5c3fe5263c92e34dc18b8135a132e839eba
Jerry Ablan <jerryablan@gmail.com>
2011-08-06 17:32:03 -0400

    -m

--- examples/blog/protected/views/comment/index.php
+++
@@ -1,8 +0,0 @@
-<h1><?php echo $this->action->id; ?> View</h1>
-
-<p>
-This is the view content for action "<?php echo $this->action->id; ?>" of controller "<?php echo $this->id; ?>".
-</p>
-<p>
-You may customize this view by editing the file "<?php echo __FILE__; ?>".
-</p>
